0a9d530f8c3512b02ca339bae29634ebb2e5360f
RicardoArquimedes <richardcqt28@gmail.com>
2021-02-15 11:17:12 -0500

    updating 4-hbnb.js for task 4

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -4,10 +4,37 @@ $(document).ready(function () {
     $.ajax({
       type: "POST",
       url: "http://0.0.0.0:5001/api/v1/places_search/",
-      data: "{}",
+      data: JSON.stringify({ "amenities": Object.keys(amenities) }),
       contentType: "application/json",
       dataType: "json",
       success: function (data) {
+        $("section.places").empty();
+        $("section.places").append(
+          data.map((place) => {
+            return `<article>
+            <div class="title_box">
+              <h2>${place.name}</h2>
+              <div class="price_by_night">${place.price_by_night}</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">${place.max_guest} Guests</div>
+                    <div class="number_rooms">${place.number_rooms}Bedrooms</div>
+                    <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+            </div>
+            <div class="user">
+                    <b>Owner:</b> ${place.user.first_name} ${place.user.last_name}
+                  </div>
+                  <div class="description">
+                    ${place.description}
+                  </div>
+          </article>`;
+          })
+        );
+      },
+    });
+
+
+
   });
 
   const url = "http://0.0.0.0:5001/api/v1/status/";
